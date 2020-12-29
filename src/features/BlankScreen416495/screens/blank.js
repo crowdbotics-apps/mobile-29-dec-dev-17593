12a74a25cdd354b7077384cf62dcdeb7875356e3
Crowdbotics <team@crowdbotics.com>
2020-12-29 13:27:55 +0000

    Update components

--- src/features/BlankScreen416495/screens/blank.js
+++ src/features/BlankScreen416495/screens/blank.js
@@ -24,18 +24,20 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = { Switch_2: true }
+  state = { Switch_2: true, Switch_19: true }
 
   render = () => (
     <View>
-      <Switch
-        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
-        style={styles.Switch_2}
-        value={this.state.Switch_2}
-        onValueChange={nextChecked => this.setState({ Switch_2: nextChecked })}
-      />
       <View>
         <View style={styles.View_5}>
+          <Switch
+            trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+            style={styles.Switch_2}
+            value={this.state.Switch_2}
+            onValueChange={nextChecked =>
+              this.setState({ Switch_2: nextChecked })
+            }
+          />
           <Image
             source={{
               uri:
@@ -44,8 +46,16 @@ export default class Blank extends React.Component {
             style={styles.Image_3}
           />
         </View>
-        <View>
+        <View style={styles.View_6}>
           <Text>Sample text content</Text>
+          <Switch
+            trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+            style={styles.Switch_19}
+            value={this.state.Switch_19}
+            onValueChange={nextChecked =>
+              this.setState({ Switch_19: nextChecked })
+            }
+          />
           <Image
             source={{
               uri:
@@ -61,11 +71,12 @@ export default class Blank extends React.Component {
 
 const styles = StyleSheet.create({
   View_1: {},
-  Switch_2: { alignSelf: "flex-start" },
   View_4: {},
   View_5: { width: 200, height: 100 },
+  Switch_2: { alignSelf: "flex-start" },
   Image_3: { width: 100, height: 100 },
-  View_6: {},
+  View_6: { width: 300, height: 200 },
   Text_8: {},
+  Switch_19: { alignSelf: "flex-start" },
   Image_9: { width: 100, height: 100 }
 })
